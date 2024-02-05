9ee628fb0ca37cb665e49d5938e880a3ab174646
Michael Woolnough <michael.woolnough@gmail.com>
2024-02-05 21:17:50 +0000

    compress and encode source into URL

--- createFragment.sh
+++ createFragment.sh
@@ -17,7 +17,18 @@ HEREDOC
 while [ $# -gt 0 ]; do
 	case "$1" in
 	"-t"|"--type")
-		type="$2";
+		type="$(echo "$2" | tr A-Z a-z)";
+
+		case "$type" in
+		"m"|"b"|"p"|"h"|"s"|"c"|"t") ;;
+		*)
+			{
+				echo "Invalid type";
+				printHelp;
+			} >&2;
+
+			exit 1;
+		esac;
 
 		shift;;
 	"-s"|"--source")
@@ -37,3 +48,26 @@ while [ $# -gt 0 ]; do
 	shift;
 done;
 
+if [ -n "$key" ]; then
+	type="$(echo "$key" | tr a-z A-Z)"
+
+	# sign doc
+	# append signature to doc
+	# append signature length to doc
+fi;
+
+declare isTmp=false;
+
+if [ "$src" = "-" ]; then
+	isTmp=true;
+
+	src="$(mktemp)";
+
+	cat > "$src";
+fi;
+
+echo "http://127.0.0.1:8080/#$(zopfli --deflate -m "$src" -c | base64 | tr -d '\n')";
+
+if $isTmp; then
+	rm -f "$src";
+fi;
