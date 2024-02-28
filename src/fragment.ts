1bddc7bf252f7ca604a57dc2dcfeecf51af36d11
Michael Woolnough <michael.woolnough@gmail.com>
2024-02-28 20:44:23 +0000

    re-add titles to table export

--- src/fragment.ts
+++ src/fragment.ts
@@ -248,7 +248,7 @@ const hash = window.location.hash.slice(1),
 		input("", {"id": "C", "type": "radio", "checked": "", "name":"E", "onclick": () => exportChar = ","}),
 		label("TSV", {"for": "T"}),
 		input("", {"id": "T", "type": "radio", "name":"E", "onclick": () => exportChar = "\t"}),
-		button("Export Table", {"onclick": () => a("", {"href": URL.createObjectURL(new Blob([(Array.from(tbodyElement.children) as HTMLTableRowElement[]).filter(e => dataMap.has(e)).map(row => dataMap.get(row)!.map(cell => `"${cell.replaceAll('"', '""')}"`).join(exportChar)).join("\n")], {"type": "text/csv;charset=utf-8"})), "download": "table.csv"}).click()})
+		button("Export Table", {"onclick": () => a("", {"href": URL.createObjectURL(new Blob([(titles.length ? titles.map(title => `"${title.replaceAll('"', '""')}"`).join(exportChar) + "\n" : "") + (Array.from(tbodyElement.children) as HTMLTableRowElement[]).filter(e => dataMap.has(e)).map(row => dataMap.get(row)!.map(cell => `"${cell.replaceAll('"', '""')}"`).join(exportChar)).join("\n")], {"type": "text/csv;charset=utf-8"})), "download": "table.csv"}).click()})
 	]);
       },
       sm = "\"",
